ce800348cfb3debcde76d730151e24ed34319568
Goldensonic96 <Goldensonic96@yahoo.com>
2021-03-22 11:11:59 -0400

    Updated Part 2
    Part 2 is updated to use Konstantin's Code.

--- HW 2/Part 2/Source.cpp
+++ HW 2/Part 2/Source.cpp
@@ -1,63 +1,96 @@
 // CSC 706 HW 2 Part 2
 // Robert Kigobe, Dominic Klusek, Novichenko Konstantin
-
-#include "masks.h"
-#include <fstream>
-#include <iostream>
+#define _USE_MATH_DEFINES
 #include <GL/glut.h> // (or others, depending on the system in use)
-#include "Canvas_freeglut.h"
-using namespace std;
+#include <cmath>
 
-char title[] = "Working with Canvas";
-Canvas window = Canvas(500, 500, title);
+// Constants
+const int WINDOW_HEIGHT = 600;
+const int WINDOW_WIDTH = 600;
+const int WINDOW_X_OFFSET = 100;
+const int WINDOW_Y_OFFSET = 100;
 
-void drawCircle(float originX, float originY, float radius, float start, float end, GLenum style) {
-	glBegin(style);
-	for (int i = start; i <= end; i += 1) {
-		glVertex2f(originX + cos((i * pi) / 180) * radius, originY + sin((i * pi) / 180) * radius);
+// Initiate
+void init(void)
+{
+	glClearColor(0.8, 0.8, 0.8, 0.0); // Set display-window color to light gray
+	glMatrixMode(GL_PROJECTION); // Set projection parameters.
+	gluOrtho2D(0.0, WINDOW_HEIGHT, 0.0, WINDOW_WIDTH);
+}
+
+// Draw a circle or part of the circle
+void drawCircle(int originX, int originY, int start, int end, int radius)
+{
+	glBegin(GL_POLYGON);
+	glVertex2d(originX, originY);
+	for (double i = start; i <= end; i++)
+	{
+		glVertex2d(originX + cos((i * M_PI) / 180) * radius, originY + sin((i * M_PI) / 180) * radius);
 	}
 	glEnd();
 }
+// Render Yin Yang centered in the screen
+void drawYinYang(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT); // Clear display window.	
 
-void drawYingYang(void) {
-	glClear(GL_COLOR_BUFFER_BIT); // Clear display window.
+	// *******************Black half circle (upper half)*******************	
+	glColor3f(0.0, 0.0, 0.0); // Set color	
+	// Center in the middle of the screen usign window height and width divided by half as a coordinates
+	double centerX = WINDOW_HEIGHT / 2, centerY = WINDOW_WIDTH / 2;
+	// Set radius to a third size of the screen
+	double	radius = WINDOW_WIDTH / 3;
+	// Render upper half circle (0 to 180 degrees)
+	drawCircle(centerX, centerY, 0, 180, radius);
 
-	// draw top half of symbol
-	glColor3f(0, 0, 0); // set color
-	drawCircle(250, 250, 200, 0, 180, GL_POLYGON);
+	// *******************White half circle (lower half)*******************
+	glColor3f(1.0, 1.0, 1.0); // Set color
+	// Render lower half circle (180 to 360 degrees)
+	drawCircle(centerX, centerY, 180, 360, radius);
 
-	// draw bottom half of symbol
-	glColor3f(1, 1, 1); // set color
-	drawCircle(250, 250, 200, 180, 360, GL_POLYGON);
+	// *******************Black circle on the left side with the radius half of the original radius*******************
+	glColor3f(0.0, 0.0, 0.0); // Set color	
+	int radius_m = radius / 2; // Set the radius to be a half of the original radius
+	centerX = (double)(WINDOW_WIDTH / 2) - radius_m; // Shift the origin to	the left
+	// Render left medium circle
+	drawCircle(centerX, centerY, 0, 360, radius_m);
 
-	// draw circle to create curve of top half
-	glColor3f(0, 0, 0); // set color
-	drawCircle(350, 250, 100, 0, 360, GL_POLYGON);
+	// *******************White circle on the right side with the radius half of the original radius*******************	
+	glColor3f(1.0, 1.0, 1.0); // Set color
+	centerX = (double)(WINDOW_WIDTH / 2) + radius_m; // shift the origin to	the right	
+	// Render right medium circle
+	drawCircle(centerX, centerY, 0, 360, radius_m);
 
-	// draw circle to create curve of bottom half
-	glColor3f(1, 1, 1); // set color
-	drawCircle(150, 250, 100, 0, 360, GL_POLYGON);
+	// *******************Black small circle on the right side with the radius 1/8 of the original radius*******************
+	glColor3f(0.0, 0.0, 0.0); // Set color
+	int radius_s = radius / 8;
+	// Render right small circle
+	drawCircle(centerX, centerY, 0, 360, radius_s);
 
-	// draw "center" dot
-	glColor3f(1, 1, 1); // set color
-	drawCircle(350, 250, 25, 0, 360, GL_POLYGON);
+	// *******************White small circle on the left side with the radius 1/8 of the original radius*******************
+	glColor3f(1.0, 1.0, 1.0); // Set color
+	centerX = (double)(WINDOW_WIDTH / 2) - radius_m; // shift the origin to	the left
+	// Render left small circle
+	drawCircle(centerX, centerY, 0, 360, radius_s);
 
-	// draw "center" dot
-	glColor3f(0, 0, 0); // set color
-	drawCircle(150, 250, 25, 0, 360, GL_POLYGON);
-
-	//draw outline
-	glColor3f(0, 0, 0); // set color
-	drawCircle(250, 250, 200, 0, 360, GL_LINE_STRIP);
-
-	glutSwapBuffers();
+	glFlush(); // Process all OpenGL routines as quickly as possible.
 }
 
 void main(int argc, char** argv)
 {
-	window.setWindow(0, 500, 0, 500);
+	glutInit(&argc, argv); // Initialize GLUT.
 
-	glutDisplayFunc(drawYingYang);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // Set display mode.
+
+	glutInitWindowPosition(WINDOW_X_OFFSET, WINDOW_Y_OFFSET); // Set top-left display-window position.
+
+	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT); // Set display-window width and height.
+
+	glutCreateWindow("Drawing Yin Yang using polygons"); // Create display window.
+
+	init(); // Execute initialization procedure.
+
+	glutDisplayFunc(drawYinYang); // Send graphics to display window.
 
 	glutMainLoop(); // Display everything and wait.
 }
