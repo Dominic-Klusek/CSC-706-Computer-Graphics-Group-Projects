23e504d8e722bb90d02935a01684d8f2f83cd7aa
Goldensonic96 <Goldensonic96@yahoo.com>
2021-03-22 20:19:23 -0400

    adding some more comments

--- HW 2/Part 1/Source.cpp
+++ HW 2/Part 1/Source.cpp
@@ -17,7 +17,7 @@ void drawPolyLineFile(char* fileName)
 	inStream.open(fileName, ios::in); // open the file
 	if (inStream.fail())
 		return;
-	//glClear(GL_COLOR_BUFFER_BIT); // clear the screen
+	
 	GLint numpolys, numLines, x, y;
 	inStream >> numpolys; // read the number of polylines
 	for (int j = 0; j < numpolys; j++) // read each polyline
@@ -36,8 +36,6 @@ void drawPolyLineFile(char* fileName)
 
 void drawRegularDino(void)
 {
-	//glClear(GL_COLOR_BUFFER_BIT); // Clear display window.
-
 	glColor3f(0.5, 0.5, 0.5); // set color
 
 	// make a loop that goes around in a circle
@@ -45,22 +43,24 @@ void drawRegularDino(void)
 	int originY = 750;
 	float radius = 150;
 	char data[] = "brontoi.dat"; // define polyline file
+	// loop to draw individual dinosaurs
 	for (int i = 0; i < 360; i += 30) {
-		//cout << originX + cos((i * pi) / 180) << ", " << originY + sin((i * pi) / 180) << endl;
-		window.setViewport(originX + cos((i * pi) / 180) * radius, originY + sin((i * pi) / 180) * radius, 1000, 1000);
+		window.setViewport(originX + cos((i * pi) / 180) * radius, originY + sin((i * pi) / 180) * radius, 1000, 1000);// set view port
+
+		// reset indentity matrix
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
+
+
 		glScaled(0.2, 0.2, 0); // scale drawing
+
+		// draw dinosar
 		drawPolyLineFile(data);
 	}
-
-	//glutSwapBuffers();
 }
 
 void drawDownDino(void)
 {
-	//glClear(GL_COLOR_BUFFER_BIT); // Clear display window.
-
 	glColor3f(0.5, 0.5, 0.5); // set color
 
 	// make a loop that goes around in a circle
@@ -71,27 +71,36 @@ void drawDownDino(void)
 	float currentAngle = -180;
 	float angleMod = 0;
 	int offsetIndex = 0;
+
+	// loop to draw individual dinosaurs
 	for (int i = 0; i < 360; i += 30) {
-		window.setViewport(originX + cos((i * pi) / 180) * radius, originY + sin((i * pi) / 180) * radius, 1000, 1000);
+		// set view port
+		window.setViewport(originX + cos((i * pi) / 180) * radius, originY + sin((i * pi) / 180) * radius, 1000, 1000); 
+
+		// move origin
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
 		glOrtho(-500, 500, -500, 500, -1, 1);
+
+		// reset indentity matrix
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
+
+		// set transformations
 		glRotatef(currentAngle, 0, 0, 1);
 		glScalef(0.2, 0.2, 0);
+
+		// draw dinosar
 		drawPolyLineFile(data);
+
+		// increment variables
 		offsetIndex++;
 		currentAngle += angleMod;
 	}
-
-	//glutSwapBuffers();
 }
 
 void draw45Dino(void)
 {
-	//glClear(GL_COLOR_BUFFER_BIT); // Clear display window.
-
 	glColor3f(0.5, 0.5, 0.5); // set color
 
 	// make a loop that goes around in a circle
@@ -102,29 +111,36 @@ void draw45Dino(void)
 	float currentAngle = -90;
 	float angleMod = 30;
 	int offsetIndex = 0;
+
+	// loop to draw individual dinosaurs
 	for (int i = 0; i < 360; i += 30) {
+		// set view port
 		window.setViewport(originX + cos((i * pi) / 180) * radius, originY + sin((i * pi) / 180) * radius, 1000, 1000);
+
+		// move origin
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
 		glOrtho(-500, 500, -500, 500, -1, 1);
-		cout << currentAngle << endl;
+
+		// reset indentity matrix
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
-		//glTranslatef(offsetX[offsetIndex], offsetY[offsetIndex], 0);
+
+		// apply transformations
 		glRotatef(currentAngle, 0, 0, 1);
 		glScalef(0.2, 0.2, 0);
+
+		// draw dinosaur
 		drawPolyLineFile(data);
+
+		// increment variables
 		offsetIndex++;
 		currentAngle += angleMod;
 	}
-
-	//glutSwapBuffers();
 }
 
 void draw4UpsideDownDino(void)
 {
-	//glClear(GL_COLOR_BUFFER_BIT); // Clear display window.
-
 	glColor3f(0.5, 0.5, 0.5); // set color
 
 	// make a loop that goes around in a circle
@@ -135,25 +151,38 @@ void draw4UpsideDownDino(void)
 	float currentAngle = -270;
 	float angleMod = 30;
 	int offsetIndex = 0;
+
+	// loop to draw individual dinosaurs
 	for (int i = 0; i < 360; i += 30) {
+		// set view port
 		window.setViewport(originX + cos((i * pi) / 180) * radius, originY + sin((i * pi) / 180) * radius, 1000, 1000);
+
+		// move origin
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
 		glOrtho(-500, 500, -500, 500, -1, 1);
+
+		// reset indentity matrix
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
+
+		// apply transformations
 		glRotatef(currentAngle, 0, 0, 1);
 		glScalef(0.2, 0.2, 0);
+
+		// draw dinosaur
 		drawPolyLineFile(data);
+
+		// increment variables
 		offsetIndex++;
 		currentAngle += angleMod;
 	}
-
-	//glutSwapBuffers();
 }
 
 void drawSections(void) {
 	glClear(GL_COLOR_BUFFER_BIT); // Clear display window.
+
+	// call all drawing functions
 	drawRegularDino();
 	drawDownDino();
 	draw45Dino();
