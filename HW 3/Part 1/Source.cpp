1aad29903ece9bdd075752cb786d5a6c9db40ea2
Goldensonic96 <Goldensonic96@yahoo.com>
2021-04-06 22:28:18 -0400

    Added window and door

--- HW 3/Part 1/Source.cpp
+++ HW 3/Part 1/Source.cpp
@@ -13,13 +13,50 @@ void display() {
 	/* draw scene */
 	glPushMatrix();
 
+	GLfloat light0_ambient[] = { 0.49, 0.55, 0.61, 1.0 };
+	GLfloat light0_diffuse[] = { 0.25f, 0.25f, 0.25f, 1.0 };
+	GLfloat light0_specular[] = { 0.8f, 0.8f, 0.8f, 1.0 };
+
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light0_ambient);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light0_diffuse);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light0_specular);
+	//glLightfv(GL_LIGHT0, GL_POSITION, light0_position);
+
+	glEnable(GL_LIGHT0);
+
+	// door
+	glPushMatrix();
+	glColor3f(0.25, 0.21, 0.04);
+	glTranslatef(-0.35, -0.35, 1.0);
+	glScalef(3, 5, 0.25);
+	glutSolidCube(0.25);
+	glPopMatrix();
+
+	// door handle
+	glPushMatrix();
+	glTranslatef(-0.10, -0.3, 1.1);
+	glutSolidSphere(0.05, 128, 128);
+	glPopMatrix();
+
 	// house
-	//GLfloat ambientLight[] = { 0.8, 0.8, 0.8, 1.0 };
-	//glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, ambientLight);
+	//GLfloat ambientLight[] = { 0.10588, 0.058824, 0.0113725};
+	//GLfloat diffuseLight[] = { 0.427451, 0.470588, 0.541176 };
+	//GLfloat specularLight[] = { 0.3333, 0.3333, 0.521569 };
+	//glMaterialfv(GL_FRONT, GL_AMBIENT, ambientLight);
+	//glMaterialfv(GL_FRONT, GL_DIFFUSE, ambientLight);
+	//glMaterialfv(GL_FRONT, GL_SPECULAR, ambientLight);
 
 	glPushMatrix();
 	glColor3f(0, 1, 0);
 	glutSolidCube(2); // building
+	glPopMatrix();
+
+	// window
+	glPushMatrix();
+	glColor3f(0.8, 0.8, 0);
+	glTranslatef(0.55, 0.35, 1.0);
+	glScaled(3, 3, 0.25);
+	glutSolidCube(0.25); // building
 	glPopMatrix();
 
 	// roof
@@ -101,7 +138,6 @@ int main(int argc, char* argv[]) {
 
 	/* turn on default lighting */
 	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
 
 	glEnable(GL_COLOR_MATERIAL);
 	glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT);
